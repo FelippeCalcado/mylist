92d12fe1bfbc051e9f1d3759757dab3206df208d
FelippeCalcado <98046356+FelippeCalcado@users.noreply.github.com>
2022-05-19 10:45:41 -0300

    basic files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mylist',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Felippe',
+    author_email='felippecalcado@gmail.com',
+    description='Another to do list'
+)
